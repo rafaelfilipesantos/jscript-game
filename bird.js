1b2fc41add064f338991f27b85a3fded33e25f09
rafaelfilipesantos <78551484+rafaelfilipesantos@users.noreply.github.com>
2021-11-15 18:18:04 +0000

    Update on Project

--- bird.js
+++ bird.js
@@ -1,13 +1,25 @@
-const birdElem = document.querySelector("data-bird");
+const birdElem = document.querySelector("[data-bird]");
 const BIRD_SPEED = 0.5;
+const JUMP_DURATION = 125;
+let timeSinceLastJump = Number.POSITIVE_INFINITY;
 
 export function setupBird() {
   setTop(window.innerHeight / 2);
+  document.removeEventListener("keydown", handleJump);
+  document.addEventListener("keydown", handleJump);
 }
 
 export function updateBird(delta) {
-  setTop(getTop() + BIRD_SPEED * delta);
-  console.log(getTop());
+  if (timeSinceLastJump < JUMP_DURATION) {
+    setTop(getTop() - BIRD_SPEED * delta);
+  } else {
+    setTop(getTop() + BIRD_SPEED * delta);
+  }
+  timeSinceLastJump += delta;
+}
+
+export function getBirdRect() {
+  return birdElem.getBoundingClientRect();
 }
 
 function setTop(top) {
@@ -16,4 +28,10 @@ function setTop(top) {
 
 function getTop() {
   return parseFloat(getComputedStyle(birdElem).getPropertyValue("--bird-top"));
+}
+
+function handleJump(e) {
+  if (e.code !== "Space") return;
+
+  timeSinceLastJump = 0;
 }
